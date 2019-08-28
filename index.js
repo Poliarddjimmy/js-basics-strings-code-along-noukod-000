10d638c349554f003a09206ca76e62cd861486de
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-08-28 14:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const currentUser = 'Grace Hopper';
  
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
  
-
-
-const shortGreeting = `Welcome, ${currentUser}!'
-;
- 
+const shortGreeting = 'Welcome, ${currentUser}!';
  
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
