0c294d75e67700f8470e9dcaf1a0505c5e012073
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-08-28 14:41:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
-const currentUser = 'Edsger Dijkstra';
+
+const shortGreeting = "" ;
  
